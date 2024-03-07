1fd5a89ff3816eded1ef8d9a54c74ade60590a11
Kauê Alcantara Rocha <118750523+Kaueroch@users.noreply.github.com>
2024-03-07 08:48:08 -0300

    Add files via upload

---
+++ listar.php
@@ -0,0 +1,12 @@
+<body>
+    <form name="dados" method="POST" action="">
+        <h1>Inclusão dos Registros</h1><h3>
+        Id <input type="text" name="txtid" size="5" maxlength="5" style="font-size: 20px;color: red;"><br>
+        Nome <input type="text" name="txtnome" size="30" maxlength="30" style="font-size: 20px;color: red;"><br>
+        Telefone <input type="text" name="txttelefone" size="30" maxlength="30" style="font-size: 20px;color: red;"><br>
+        Email <input type="text" name="txtemail" size="50" maxlength="50" style="font-size: 20px;color: red;"><br><br>
+        <input type="button" name="btngravar" value="Gravar" style="font-size: 20px;color: blue;">
+        <input type="reset" name="btnlimpar" value="Limpar" style="font-size: 20px;color: blue;"><br>
+    </form>
+</body>
+<?php
